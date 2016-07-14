1aa6fb3a4732e7af15f0b9a361dfd867ca987a9b
Dylan <dylanslack@gmail.com>
2016-07-14 15:16:18 -0600

    Update modals demo

--- demo/demos/Modals.js
+++ demo/demos/Modals.js
@@ -1,62 +1,61 @@
 /* eslint-disable max-len */
 import React from 'react';
 import {
-  Button,
-  Tooltip,
   BasicModal,
+  Button,
+  ButtonGroup,
   ConfirmModal,
-  Modal,
+  Container,
   Content,
+  Col,
+  Modal,
+  Row,
+  Tooltip,
   Trigger,
 } from '../../src';
 
-const onConfirm = () => new Promise(resolve => setTimeout(resolve, 1000));
+const onConfirm = () => new Promise(() => alert('Done!'));
 
 const ModalDemo = () => (
-  <div style={{ padding: '2rem' }}>
-    <h1>Modals</h1>
-    <Modal>
-      <Trigger>
-        <Button color="primary">Trigger and content modal</Button>
-      </Trigger>
-      <Content>
-        <BasicModal title="Basic Modal">
-          <p>this is a basic modal</p>
-          <Tooltip text="A tooltip man!" attachment="bottom center">
+  <Container>
+    <Row>
+      <Col>
+        <h2>Modals</h2>
+        <hr></hr>
+        <ButtonGroup>
+          <Modal>
             <Trigger>
-              <Button>Tooltip</Button>
+              <Button color="primary">Basic modal</Button>
             </Trigger>
             <Content>
-              <div>i am a tooltip</div>
+              <BasicModal title="Basic Modal">
+                <p>this is a basic modal</p>
+                <Tooltip text="A tooltip man!" attachment="bottom center">
+                  <Trigger>
+                    <Button>Tooltip</Button>
+                  </Trigger>
+                  <Content>
+                    <div>i am a tooltip</div>
+                  </Content>
+                </Tooltip>
+              </BasicModal>
             </Content>
-          </Tooltip>
-        </BasicModal>
-      </Content>
-    </Modal>
-  </div>
+          </Modal>
+          <Modal>
+            <Trigger>
+              <Button color="primary">Confirm modal</Button>
+            </Trigger>
+            <Content>
+              <ConfirmModal
+                title="Are you sure you want to do this?"
+                onConfirm={onConfirm}
+              />
+            </Content>
+          </Modal>
+        </ButtonGroup>
+      </Col>
+    </Row>
+  </Container>
 );
-
-/*
-const old = (
-  <div>
-    <Modal target={<Button type="primary">Basic modal</Button>}>
-      <BasicModal title="Basic Modal">
-        <p>this is a basic modal</p>
-        <Tooltip text="A tooltip man!" attachment="bottom center">
-          <Button>Tooltip</Button>
-        </Tooltip>
-      </BasicModal>
-    </Modal>
-    <Modal target={<Button type="primary">Big modal</Button>}>
-      <BasicModal title="Big Modal" size="lg">
-        <p>this is a big modal</p>
-      </BasicModal>
-    </Modal>
-    <Modal target={<Button type="primary">Confirm modal</Button>}>
-      <ConfirmModal title="Are sure you want to do this?" onConfirm={onConfirm} />
-    </Modal>
-  </div>
-);
-*/
 
 export default ModalDemo;
