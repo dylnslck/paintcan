d9e3f1e7a395977106b97309bb4fe170f292a516
Dylan <dylanslack@gmail.com>
2016-07-26 17:08:29 -0600

    Expose withTether export

--- src/wrappers/index.js
+++ src/wrappers/index.js
@@ -1,4 +1,5 @@
 export { default as withDropdown } from './withDropdown';
 export { default as withModal } from './withModal';
 export { default as withSelect } from './withSelect';
+export { default as withTether } from './withTether';
 export { default as withTooltip } from './withTooltip';
